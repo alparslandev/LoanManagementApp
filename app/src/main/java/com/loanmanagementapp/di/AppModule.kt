71fa24859d03518a9c7cb28c8e5cea29b8b4d2da
AlparslanDev <alp.develioglu@gmail.com>
2025-03-29 00:13:08 +0300

    Update DI with new strategy classes

--- app/src/main/java/com/loanmanagementapp/di/AppModule.kt
+++ app/src/main/java/com/loanmanagementapp/di/AppModule.kt
@@ -1,12 +1,18 @@
 package com.loanmanagementapp.di
 
-import com.loanmanagementapp.data.LoanRepository
-import com.loanmanagementapp.data.LoanService
-import com.loanmanagementapp.data.MockLoanService
+import com.loanmanagementapp.data.repository.LoanRepository
+import com.loanmanagementapp.data.repository.LoanService
+import com.loanmanagementapp.data.repository.MockLoanService
+import com.loanmanagementapp.data.strategy.DefaultLoanStrategy
+import com.loanmanagementapp.data.strategy.DueDateStrategy
+import com.loanmanagementapp.data.strategy.LoanStrategySelector
+import com.loanmanagementapp.data.strategy.LoanUpdateStrategy
+import com.loanmanagementapp.data.strategy.PaidLoanStrategy
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import dagger.multibindings.IntoSet
 import javax.inject.Singleton
 
 @Module
@@ -17,6 +23,27 @@ object AppModule {
     fun provideLoanService(): LoanService = MockLoanService()
 
     @Provides
+    @IntoSet
+    fun provideDueDateStrategy(): LoanUpdateStrategy = DueDateStrategy()
+
+    @Provides
+    @IntoSet
+    fun providePaidLoanStrategy(): LoanUpdateStrategy = PaidLoanStrategy()
+
+    @Provides
+    @IntoSet
+    fun provideDefaultLoanStrategy(): LoanUpdateStrategy = DefaultLoanStrategy()
+
+    @Provides
     @Singleton
-    fun provideLoanRepository(loanService: LoanService): LoanRepository = LoanRepository(loanService)
+    fun provideLoanStrategySelector(
+        strategies: Set<@JvmSuppressWildcards LoanUpdateStrategy>
+    ): LoanStrategySelector = LoanStrategySelector(strategies.toList())
+
+    @Provides
+    @Singleton
+    fun provideLoanRepository(
+        loanService: LoanService,
+        strategySelector: LoanStrategySelector
+    ): LoanRepository = LoanRepository(loanService, strategySelector)
 }
